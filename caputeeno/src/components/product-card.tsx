6f5501bd6118ba5d18493be53ff5b53abc89ad40
DavidLucasK <davidlucasfr70@gmail.com>
2023-08-15 12:49:26 -0300

    Adding navigation product page

--- caputeeno/src/components/product-card.tsx
+++ caputeeno/src/components/product-card.tsx
@@ -1,10 +1,12 @@
 import { formatPrice } from "@/utils/format-price"
 import { styled } from "styled-components"
+import { useRouter } from "next/navigation"
 
 interface ProductCardProps {
     image: string,
     title: string,
     price: number,
+    id: string
 }
 
 const Card = styled.div`
@@ -12,6 +14,7 @@ const Card = styled.div`
     align-items: center;
     justify-content: center;
     flex-direction: column;
+    cursor: pointer;
 
     background: rgba(255, 255, 255, 0.4);
     backdrop-filter: blur(10px);
@@ -58,10 +61,15 @@ const Card = styled.div`
 `
 
 export function ProductCard(props: ProductCardProps){
+    const router = useRouter();
     const price = formatPrice(props.price)
 
+    const handleNavigate = () => {
+        router.push("/product?id=" + props.id);
+    }
+
     return(
-        <Card>
+        <Card onClick={handleNavigate}>
                 <img src={props.image}/>
                 <div>
                     <h3>{props.title}</h3>
